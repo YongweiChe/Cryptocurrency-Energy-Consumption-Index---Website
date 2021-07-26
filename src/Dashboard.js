e0b706197925fb58450ca90cd3487fa82e5f1b0a
Yongwei Che <yongwei.che02@gmail.com>
2021-07-25 21:13:27 -0400

    added block info

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,10 +4,21 @@ import axios from 'axios'
 
 const Dashboard = () => {
     const [codes, setCodes] = useState([])
+    const [infos, setInfos] = useState([])
     useEffect(async () => {
-        const url = 'https://crypto-energy-api.herokuapp.com/coins'
+        const url = 'https://crypto-energy-api.herokuapp.com/coins';
         const response = await axios.get(url);
-        setCodes(response.data)
+        const c = response.data
+        let list = c[0].code;
+        for (let i = 1; i < c.length; i++) {
+            list += ',' + c[i].code
+        }
+        const url2 = 'https://api.minerstat.com/v2/coins?list=' + list;
+        const response2 = await axios.get(url2);
+        console.log(response2.data)
+        console.log(c)
+        setCodes(c)
+        setInfos(response2.data)
     }, [])
 
 
@@ -16,9 +27,26 @@ const Dashboard = () => {
             <h1>The Cryptocurrency Energy Consumption Index</h1>
 
             <p>
-                {codes.map(info => {
+                {infos.map((info, i) => {
+                    function isCorrect(myapi) {
+                        return myapi.code === info.coin
+                    }
+                    let myCode = codes.find(isCorrect)
+                    if (!myCode.time) {
+                        return (
+                            <span></span>
+                        )
+                    }
+                    if (info.algorithm.toLowerCase().substring(0,3) !== myCode.algo.toLowerCase().substring(0,3)) {
+                        console.log(info.algorithm)
+                        console.log(codes.find(isCorrect).algo)
+                        return (
+                            <span></span>
+                        )
+                    }
+                    
                     return (
-                        <Card code={info.code} />
+                        <Card key={i} code={info.coin} info={info}/>
                     )
                 })}
             </p>
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,36 +2,37 @@ import React, {useState, useEffect} from 'react'
 
 import axios from 'axios'
 
-const Card = (props) => {
+const Card = ({code, info}) => {
     const [coin, setCoin]= useState([])
     const [pools, setPools] = useState([])
     const [miners, setMiners] = useState([])
 
     useEffect(async ()=> {
-        const url = 'https://crypto-energy-api.herokuapp.com/coins/' + props.code
+        const url = 'https://crypto-energy-api.herokuapp.com/coins/' + code
         const response = await axios.get(url);
         setCoin(response.data.info)
-        console.log(response.data.info)
         setPools(response.data.pools)
-        console.log(response.data.pools)
         setMiners(response.data.miners)
     }, [])
 
 
     return (
         <div className="card">
-            <h2><b>{coin.name}:</b></h2>
+            <h2><b>{coin.name} ({info.coin}):</b></h2>
             <h3>
-                Hashrate: {coin.network_hashrate}
+                Algorithm: {info.algorithm}<br/>
+                Hashrate: {info.network_hashrate === -1 ? coin.network_hashrate : info.network_hashrate}<br/>
+                Block Time: {coin.time}<br/>
+                Block Reward: {coin.reward}
             </h3>
             <h3>
-                Price: {coin.price}
+                Price: ${info.price === -1 ? coin.price : info.price}
             </h3>
             <p>
                 <span><b>Pools: </b></span>
                 {pools.map((pool, i) => {
                     return (
-                        <span key={i}>{pool.url}: <em>{pool.hashrate}</em>      |      </span>
+                        <span key={i}>{pool.url}: <em>{pool.hashrate}</em>, {pool.countries}     |      </span>
                     )
                 })}
             </p>
@@ -39,7 +40,7 @@ const Card = (props) => {
                 <span><b>Miners: </b></span>
                 {miners.map(miner => {
                     return (
-                        <span>{miner.name}, </span>
+                        <span>{miner.name}: {miner.hashrate} {miner.power} | </span>
                     )
                 })}
             </p>
